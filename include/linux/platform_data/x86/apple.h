8ecd67c72129df64a1985efda589844eb03be803
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2021-02-16 12:04:12 +0100

    wip: properly prefix struct and enum kernel-doc descriptions in include/linux/platform_data/x86/apple.h

--- include/linux/platform_data/x86/apple.h
+++ include/linux/platform_data/x86/apple.h
@@ -3,7 +3,7 @@
 
 #ifdef CONFIG_X86
 /**
- * x86_apple_machine - whether the machine is an x86 Apple Macintosh
+ * bool x86_apple_machine - whether the machine is an x86 Apple Macintosh
  */
 extern bool x86_apple_machine;
 #else
